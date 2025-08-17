13f2e277169bd6637430274de53d9283d4db6bca
Shaurya Awasthi <shaurya.awasthi130@gmail.com>
2025-08-18 01:21:14 +0530

    resend otp testing

--- src/app.js
+++ src/app.js
@@ -13,6 +13,13 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
 
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true
+    })
+)
+
 // 2. Session Middleware
 app.use(
     session({
